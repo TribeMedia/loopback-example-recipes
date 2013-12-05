e0e16dde0246d4709bd8c28f63adfc67f1d7fea0
Raymond Feng <raymond@strongloop.com>
2013-12-05 13:48:00 -0800

    Update deps

--- recipes/model-from-discovery.js
+++ recipes/model-from-discovery.js
@@ -8,9 +8,6 @@ var ds = require('../data-sources/db.js')('oracle');
  */
 ds.discoverAndBuildModels('INVENTORY', {visited: {}, owner: 'LOOPBACK', associations: true}, function (err, models) {
 
-    console.log(ds.name, ds.connector.name);
-    console.log(models.Inventory.settings);
-    console.log(models.Inventory.definition.properties);
     models.Inventory.findOne({}, function (err, inv) {
         if (err) {
             console.error(err);
